baaaa6f2b38f37c8b8ed384b8416800cde55fd50
Will Ricketts <william.w.ricketts@gmail.com>
2015-11-21 15:11:18 +0000

    adding listener view

--- archer.js
+++ archer.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.sendFile('./public/index.html');
+  res.render('index');
 });
 
 var server = app.listen(process.env.PORT, function () {
