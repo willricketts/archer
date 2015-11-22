c5db46bf92cedfcf0e8e2f099c2d9fd90683a689
Will Ricketts <will@willricketts.com>
2015-11-22 18:05:22 +0000

    changing structure

--- server/archer.js
+++ server/archer.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', '../listener_node/public/views');
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.render('index');
