6e68d8708db19ebae8ef2c3ae4b4d8048bef0374
Will Ricketts <will@willricketts.com>
2015-11-23 01:37:03 +0000

    adding socket support

--- server/archer.js
+++ server/archer.js
@@ -1,17 +1,33 @@
 var express = require('express');
+var request = require('request');
 var app = express();
+var io = require('socket.io')(8081);
+
+io.sockets.on('connection', function (socket) {
+  console.log('connection');
+});
+
+var io_client = require('socket.io-client');
+var socket = io_client('');
+
+socket.on('connect', function(data) {
+    console.log('connect');
+});
 
 app.get('/', function (req, res) {
   res.json(process.memoryUsage());
 });
 
 app.post('/', function(req, res) {
-  
+  request.get('', function(err, res) {
+    if(err) {
+      res.error(err);
+    }
+    console.log(res.body);
+  });
 });
 
 var server = app.listen(process.env.PORT, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
+  var port = process.env.PORT
   console.log('Archer Server listening on %s', port);
 });
--- worker/worker.js
+++ worker/worker.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
+var io = require('socket.io')(8081);
+
+io.sockets.on('connection', function(data) {
+    console.log('received connection');
+});
+
+var io_client = require('socket.io-client');
+var socket = io_client('');
+
+socket.on('connect', function(data) {
+    console.log('connect');
+    socket.emit('join', 'Hello World from client');
+});
 
 app.set('view engine', 'ejs');
-app.set('views', './public/views');
+app.set('views', 'public/views');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
